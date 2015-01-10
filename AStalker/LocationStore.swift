03f68c0b6ce2abc6bb57c5a0dff4e52636382c6a
Basti <bastian.morath@gmx.ch>
2015-01-10 15:42:51 +0100

    LocalStore erweitert

--- AStalker/LocationStore.swift
+++ AStalker/LocationStore.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreData
 
 class LocationStore: NSObject{
     var coreDataStore: ACoreDataStore = ACoreDataStore.defaultStore()
@@ -31,11 +32,69 @@ class LocationStore: NSObject{
     }
     
     /**************************** READ Methods **********************************/
-    func getContacts() -> [User]{
+    func getUser() -> [User]{
         return self.coreDataStore.performFetch("User") as [User]
     }
     
+    /*
+    :returns: Returns a NSFetchedResultsController for all User.
+    */
+    func getUserFetchedResultsController() -> NSFetchedResultsController {
+        // sort them alphabetically
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true )
+        
+        return coreDataStore.createFetchedResultsController("User", predicate: nil, sortDescriptors: [sortDescriptor] )
+    }
+    
+    //TODO: LocalUser verbessern, nil prüfen
+    func getLocalUser() -> LocalUser?{
+        var localUserArray = self.coreDataStore.performFetch("LocalUser") as [LocalUser]
+       return localUserArray.first
+    }
+    
+    func getContacts(){
+        
+    }
+    
+    
+    
     /**************************** WRITE Methods **********************************/
-
-
+    func createUser(name: String) -> User? {
+        var userObject = self.coreDataPortal.createObject("User") as User?
+        if let user = userObject {
+            user.name = name
+            return user
+        }
+        return nil
+    }
+    
+    func createLocalUser(name: String, phoneNumber: String) -> LocalUser?{
+        var user = self.coreDataPortal.createObject("LocalUser") as LocalUser?
+        if let localUser = user {
+            localUser.phoneNumber = phoneNumber
+            return localUser
+        }
+        return nil
+    }
+    
+    /**
+    Erstellt eine Location mit Name, Timestamp, Longitude und latitude.
+    Der User ist optional: Wird einer mitgeliefert, wird die Location dem Property 'sharedLocation' des Users, anderfalls des LcoalUsers hinzugefügt.
+    */
+    func createLocation(name: NSString, timestamp: NSDate, longitude: Double, latitude: Double, user: User? = nil) -> Location?{
+        var locationObject = self.coreDataPortal.createObject("Location") as Location?
+        if let location = locationObject {
+            location.name = name
+            location.timestamp = timestamp
+            location.longitude = longitude
+            location.latitude = latitude
+            if (user != nil) {
+                user!.sharedLocations = user!.sharedLocations.setByAddingObject(location)
+            } else {
+                
+            }
+            return location
+        }
+        return nil
+    }
 }
